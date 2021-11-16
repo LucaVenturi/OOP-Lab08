6c26489efb685d8eb36b4168d3ccca5976a6263a
Luca <luca.venturi29@studio.unibo.it>
2021-11-16 16:55:15 +0100

    Made controller implement Serializable

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -5,11 +5,13 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.Serializable;
 
 /**
- * 
+ *
  */
-public class Controller {
+public class Controller implements Serializable {
+    private static final long serialVersionUID = -1295964518006405011L;
     private File file;
     /*
      * This class must implement a simple controller responsible of I/O access. It
