30950fad9f23cba4515d621b2a22ba44f4cb1958
Luca <luca.venturi29@studio.unibo.it>
2021-11-16 16:53:33 +0100

    Created Controller class. TODO check warnings on files.

--- src/it/unibo/oop/lab/mvcio/Controller.java
+++ src/it/unibo/oop/lab/mvcio/Controller.java
@@ -1,10 +1,16 @@
 package it.unibo.oop.lab.mvcio;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 /**
  * 
  */
 public class Controller {
-
+    private File file;
     /*
      * This class must implement a simple controller responsible of I/O access. It
      * considers a single file at a time, and it is able to serialize objects in it.
@@ -27,5 +33,47 @@ public class Controller {
      * System.getProperty("file.separator"). The combined use of those methods leads
      * to a software that runs correctly on every platform.
      */
+    public Controller() {
+        super();
+        this.file = new File(System.getProperty("user.home") + System.getProperty("file.separator") + "output.txt");
+    }
+    public Controller(final File file) {
+        super();
+        this.file = file;
+    }
+    /**
+     * @return
+     *      returns current file.
+     */
+    public File getFile() {
+        return file;
+    }
+    /**
+     * @param file
+     *      The file that will be used by the class.
+     */
+    public void setFile(final File file) {
+        this.file = file;
+    }
+    /**
+     * @return
+     *      Path of the current file.
+     */
+    public String getPath() {
+        return this.file.getPath();
+    }
+    /**
+     * @param s
+     *      String to write in the file
+     * @throws IOException
+     */
+    public void writeOnFile(final String s) throws IOException {
+        final OutputStream file = new FileOutputStream(this.file);
+        final DataOutputStream out = new DataOutputStream(file);
+        out.writeUTF(s);
+        out.close();
+        file.close();
+
+    }
 
 }
